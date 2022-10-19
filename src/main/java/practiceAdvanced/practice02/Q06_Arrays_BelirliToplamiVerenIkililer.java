41d1194d88e44d3f453609d20bc2f3011d060995
Mustafa Lafcı <115083814+mustafalafci@users.noreply.github.com>
2022-10-19 23:32:14 +0300

    besinci

---
+++ src/main/java/practiceAdvanced/practice02/Q06_Arrays_BelirliToplamiVerenIkililer.java
@@ -0,0 +1,15 @@
+package practiceAdvanced.practice02;
+
+public class Q06_Arrays_BelirliToplamiVerenIkililer {
+    public static void main(String[] args) {
+        /*
+     Toplamları, verilen bir sayıya eşit olan  Array element çiflerini yazdıran bir method oluşturunuz.
+     {4,6,5,-10,8,5,20} ===> 10
+     4 + 6 = 10
+     5 + 5 = 10
+    -10 + 20 = 10
+     */
+
+    }
+
+}
