4402a65b007735866e14bcea469d99720112c340
Mustafa Lafcı <115083814+mustafalafci@users.noreply.github.com>
2022-10-26 20:59:40 +0300

    guncel

--- src/main/java/practiceAdvanced/practice02/Q06_Arrays_BelirliToplamiVerenIkililer.java
+++ src/main/java/practiceAdvanced/practice02/Q06_Arrays_BelirliToplamiVerenIkililer.java
@@ -9,6 +9,26 @@ public class Q06_Arrays_BelirliToplamiVerenIkililer {
      5 + 5 = 10
     -10 + 20 = 10
      */
+        int[] arr={4,6,5,-10,8,5,20};
+        ikilileriBul(arr,10);
+    }
+
+    public static void ikilileriBul(int[] arr,int number){
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+
+                if (arr[i]+arr[j]==number){
+
+                    System.out.println((arr[i] + " " + arr[j] + "=" + number));
+                }
+            }
+        }
+
+
+
+
+
 
     }
 
